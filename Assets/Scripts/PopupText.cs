9287391b57f48f331d2c16f3262faa2168ba9020
Nonoho Taniguchi <taniho_0707@yahoo.co.jp>
2014-11-08 16:14:27 +0900

    Renamed Draw

---
+++ Assets/Scripts/PopupText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopupText : MonoBehaviour {
+
+    void Start() {
+
+    }
+
+	void Update() {
+
+	}
+
+    void Draw(string text) {
+        
+    }
+
+}
