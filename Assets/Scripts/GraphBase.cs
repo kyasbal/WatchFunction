99b55f81f0a97ee08a4eeeca81c520e438d659d0
Nonoho Taniguchi <taniho_0707@yahoo.co.jp>
2014-11-08 23:48:29 +0900

    タッチ座標を取得するクラスを実装

---
+++ Assets/Scripts/GraphBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphBase : MonoBehaviour {
+
+    private Vector2 vect;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Touch touch;
+        if (Input.touchCount > 0) {
+            touch = Input.GetTouch(0);
+            vect.x = 2 * touch.position.x / GameObject.renderer.bounds.size.x - 1;
+            vect.y = 2 * touch.position.y / GameObject.renderer.bounds.size.y - 1;
+        }
+	}
+
+    public Vector2 mouseVector2 { get; }
+
+}
