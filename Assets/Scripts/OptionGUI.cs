7d10cff066206c0242e0314794240960f2fff04c
ArLE <arlequin.prx@gmail.com>
2014-11-09 00:35:50 +0900

    ボタンの修正

---
+++ Assets/Scripts/OptionGUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionGUI : ButtonGUI_Base {
+
+	protected override void SetButtonActive() {
+		actButton[0] = true;
+		actButton[1] = true;
+		actButton[2] = true;
+		actShareButton = false;
+	}
+
+	protected override void DoMove(int i) {
+		switch (i) {
+			case 0:	//へ
+				break;
+
+			case 1:	//へ
+				break;
+
+			case 2:	//へ
+				break;
+
+			default:
+				throw new System.NotImplementedException();
+		}
+	}
+}
