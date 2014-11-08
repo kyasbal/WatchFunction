3c325ad080ed9c930eb862bbd2ad465317043ac8
Nonoho Taniguchi <taniho_0707@yahoo.co.jp>
2014-11-08 17:19:44 +0900

    FunctionFactoryを追加

---
+++ Assets/Scripts/FunctionFactory.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FunctionFactory : MonoBehaviour {
+
+	public int difficulty = 0;
+
+	//private IFunction 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IFunction getFunction(){
+
+	}
+}
