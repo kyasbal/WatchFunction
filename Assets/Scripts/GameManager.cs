c0cec2d197ee5c2351a98ac2ae6566ae28f179e2
LimeStreem <LimeStreem@gmail.com>
2014-11-08 17:19:30 +0900

    ゲームシーンの骨組みの作成

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class GameManager : MonoBehaviour {
+	public GameStatus CurrentStatus;
+
+	// Use this for initialization
+	void Start () {
+		CurrentStatus = GameStatus.GameStarting;
+	}
+	void Update () {
+		switch (CurrentStatus) {
+		case GameStatus.GameStarting:
+			break;
+		case GameStatus.GameFinishing:
+			break;
+		case GameStatus.Waiting:
+			break;
+		case GameStatus.Showing:
+			break;
+		case GameStatus.Dismissing:
+			break;
+		default:
+			throw new System.ArgumentOutOfRangeException ();
+		}
+	}
+}
