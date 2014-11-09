4839b207bf929de1236cebef4cbb0b89ec4f2ec9
LimeStreem <LimeStreem@gmail.com>
2014-11-09 17:26:36 +0900

    Fixed compile error

--- Assets/Scripts/FunctionFactory.cs
+++ Assets/Scripts/FunctionFactory.cs
@@ -16,7 +16,7 @@ public class FunctionFactory : MonoBehaviour {
         new CosFunction(),
 		new LogFunction(),
         new SecantFunction()
-        ,new Discrete1Function()
+        ,new Discrete1Function(),
         new TanFunction()
 	};
 
