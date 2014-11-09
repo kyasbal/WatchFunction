509999bbf08c33a095d1a1c5a15ee904d85e945a
LimeStreem <LimeStreem@gmail.com>
2014-11-09 14:14:22 +0900

    no message

---
+++ Assets/Scripts/Functions/PrefabController/SinFunctionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Functions.PrefabController
+{
+    class SinFunctionController:FunctionControllerBase
+    {
+        public float A;
+
+        public float B;
+
+        public float C;
+        protected override void OnUpdateFunction()
+        {
+            renderer.material.SetFloat("_A", this.A);
+            renderer.material.SetFloat("_B", this.B);
+            renderer.material.SetFloat("_C", this.C);
+        }
+    }
+}
