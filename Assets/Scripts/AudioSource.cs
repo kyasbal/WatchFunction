b22001585908fd3c9d074b923c0b85a6dc209e5f
921starfish <yuya.921.hoshino@gmail.com>
2014-11-09 16:34:50 +0900

    BGMを実装

---
+++ Assets/Scripts/AudioSource.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSource : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
